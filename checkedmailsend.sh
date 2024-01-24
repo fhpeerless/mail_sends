9038a3a04d9b4addd2d939d8f780cd54ae3e400b
fhpeerless <126315532+fhpeerless@users.noreply.github.com>
2024-01-24 14:20:06 +0800

    Update checkedmailsend.sh

--- checkedmailsend.sh
+++ checkedmailsend.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-response=$(curl -s 'https://ec2-18-218-197-117.us-east-2.compute.amazonaws.com/balance?address=0x08a275F54FBdC0B627968E3dCa789de839fbc')
+response=$(curl -s 'https://ec2-18-218-197-117.us-east-2.compute.amazonaws.com/balance?address=0x4b570b98FC419992CfFFa7fe92f011A4741A1Da1')
 
 if [ $? -ne 0 ]; then
-  echo "网络错误" | mailx -s "请求失败" 68208932@qq.com
+  echo "network" | mailx -s "error" 68208932@qq.com
 else
   balance=$(echo $response | jq -r '.balance')
-  echo "您的余额为 $balance" | mailx -s "余额信息" 68208932@qq.com
+  echo "mount is $balance" | mailx -s "yu,e" 68208932@qq.com
 fi
