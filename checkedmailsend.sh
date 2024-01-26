2c9b78645d4bda862f8ad3fdf1b1d8edabb5bcd0
fhpeerless <126315532+fhpeerless@users.noreply.github.com>
2024-01-26 16:16:32 +0800

    Update checkedmailsend.sh

--- checkedmailsend.sh
+++ checkedmailsend.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 API_URL="https://ec2-18-218-197-117.us-east-2.compute.amazonaws.com/balance?address=0x4b570b98FC419992CfFFa7fe92f011A4741A1Da1"
-ERROR_EMAIL="682808932@qq.com"
-SUCCESS_EMAIL="68208932@qq.com"
+
 
 RESPONSE=$(curl -s $API_URL)
 STATUS=$?
@@ -10,11 +9,11 @@ STATUS=$?
 if [ $STATUS -eq 0 ]; then
     BALANCE=$(echo $RESPONSE | jq '.balance')
     if [ ! -z "$BALANCE" ]; then
-        echo "yu，e，shi: $BALANCE" | mail -s "export" $SUCCESS_EMAIL
+        echo "yu，e，shi: $BALANCE" | mail -s "export" 68208932@qq.com
     else
-        echo "APIno answer。" | mail -s "Api error" $ERROR_EMAIL
+        echo "APIno answer。" | mail -s "Api error" 68208932@qq.com
     fi
 else
-    echo "can not API." | mail -s "network error" $ERROR_EMAIL
+    echo "can not API." | mail -s "network error" 68208932@qq.com
 fi
 # mail -v是显示传递细节
