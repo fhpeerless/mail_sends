dda741ed95c43bbbabca9af4eb3134f2d5c3b2af
fhpeerless <126315532+fhpeerless@users.noreply.github.com>
2024-01-28 07:28:42 +0800

    Update checkedmailsend.sh

--- checkedmailsend.sh
+++ checkedmailsend.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 export API_URL="https://ec2-18-218-197-117.us-east-2.compute.amazonaws.com/balance?address=0x4b570b98FC419992CfFFa7fe92f011A4741A1Da1"
+export YOUX="68208932@qq.com"
+
+ echo -n | openssl s_client -connect ec2-18-218-197-117.us-east-2.compute.amazonaws.com:443 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > ~/.certs/q123.crt
+ # 安装查询网址的证书
 
 # 增加了 `-m 30`，设置超时时间为 30 秒
 # RESPONSE=$(curl -k -s -m 30 $API_URL)
@@ -10,12 +14,12 @@ STATUS=$? # status测得上一个api查询链接是否返回成功
 if [ $STATUS -eq 0 ]; then  # eq为等于，
     BALANCE=$(echo $RESPONSE | jq '.balance')
     if [ ! -z "$BALANCE" ]; then
-        echo "yu.e.shi: $BALANCE" | mail -s "export" 68208932@qq.com
+        echo "yu.e.shi: $BALANCE" | mail -s "export" $YOUX
     else
-        echo "APIno=answer" | mail -s "Api error" 68208932@qq.com
+        echo "APIno=answer" | mail -s "Api error" $YOUX
     fi
 else
-    echo "can not API." | mail -s "network error" 68208932@qq.com
+    echo "can not API." | mail -s "network error" $YOUX
 fi
 
 
