2e4b7e4cfe6b19f131f5e422e2eabf401100a0c4
fhpeerless <126315532+fhpeerless@users.noreply.github.com>
2024-01-28 07:24:17 +0800

    Update checkedmailsend.sh

--- checkedmailsend.sh
+++ checkedmailsend.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-API_URL="https://ec2-18-218-197-117.us-east-2.compute.amazonaws.com/balance?address=0x4b570b98FC419992CfFFa7fe92f011A4741A1Da1"
+export API_URL="https://ec2-18-218-197-117.us-east-2.compute.amazonaws.com/balance?address=0x4b570b98FC419992CfFFa7fe92f011A4741A1Da1"
 
 # 增加了 `-m 30`，设置超时时间为 30 秒
 # RESPONSE=$(curl -k -s -m 30 $API_URL)
